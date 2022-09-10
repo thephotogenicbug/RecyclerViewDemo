9f715c3d787a99ebc4cce0ce21259628137bbe35
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-10 23:43:32 +0530

    SetOnClickListener and Display ToastMessage

--- app/src/main/java/com/naveen/recyclerviewdemo/MyRecyclerViewAdaptor.kt
+++ app/src/main/java/com/naveen/recyclerviewdemo/MyRecyclerViewAdaptor.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class MyRecyclerViewAdaptor(val fruitsList:List<Fruit>) : RecyclerView.Adapter<MyViewHolder>(){
@@ -27,6 +28,13 @@ class MyViewHolder(val view:View):RecyclerView.ViewHolder(view){
     fun bind(fruit:Fruit){
         val myTextView = view.findViewById<TextView>(R.id.tvName)
         myTextView.text = fruit.name
+
+        view.setOnClickListener{
+            Toast.makeText(
+                view.context,"Selected Fruit is : ${fruit.name}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
 }
